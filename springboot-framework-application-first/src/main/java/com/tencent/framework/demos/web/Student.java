68f582a68d1843b9fbf412b785272edce4b31ca9
liusujan-ui <liusujan6@gmail.com>
2025-11-08 00:08:08 +0800

    20251107 第一次将学习的脚本提交-student类添加V1.0.0.0

--- springboot-framework-application-first/src/main/java/com/tencent/framework/demos/web/Student.java
+++ springboot-framework-application-first/src/main/java/com/tencent/framework/demos/web/Student.java
@@ -1,9 +1,11 @@
 package com.tencent.framework.demos.web;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Student {
     private String id;
     private String name;
