8c1486a4e5d92f97618475fb67f2515b41b46d99
liusujan-ui <liusujan6@gmail.com>
2025-11-12 00:53:18 +0800

    20251111 v1.0.0.2 JPA的pageable和mybatis的pagehelper实现分页

---
+++ springboot-framework-page-seven/src/main/java/com/tencent/springbootframeworkpageseven/SpringbootFrameworkPageSevenApplication.java
@@ -0,0 +1,13 @@
+package com.tencent.springbootframeworkpageseven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFrameworkPageSevenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFrameworkPageSevenApplication.class, args);
+    }
+
+}
---
+++ springboot-framework-page-seven/src/test/java/com/tencent/springbootframeworkpageseven/SpringbootFrameworkPageSevenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tencent.springbootframeworkpageseven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFrameworkPageSevenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
