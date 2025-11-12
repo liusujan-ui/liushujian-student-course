dc7e97acd33d862793e1adb52972c0576c4f0db7
liusujan-ui <liusujan6@gmail.com>
2025-11-12 23:22:38 +0800

    20251112 v1.0.1.0添加freemaker前端模板引擎

---
+++ springboot-frontTemplates-freeMaker-two/src/test/java/com/tencent/springbootfronttemplatesfreemakertwo/SpringbootFrontTemplatesFreeMakerTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tencent.springbootfronttemplatesfreemakertwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFrontTemplatesFreeMakerTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
