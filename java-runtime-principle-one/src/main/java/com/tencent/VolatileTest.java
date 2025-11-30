6118182996b6f9d6f6fff5467934d07474b2eb24
liusujan-ui <liusujan6@gmail.com>
2025-11-30 13:18:05 +0800

    20251130 java-runtime-principle-one中添加了volatile关键字，对线程可见性知识

---
+++ java-runtime-principle-one/src/main/java/com/tencent/VolatileTest.java
@@ -0,0 +1,39 @@
+package com.tencent;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author 观自在
+ * @description JMM可见性问题
+ * volatile:可见性问题：让一个线程对共享变量的修改，能够及时的被其他线程看到。
+ * 1.禁止缓存
+ * 2.对volatile变量相关的指令不做重排序
+ *
+ * @date 2025-11-29 19:17
+ */
+public class VolatileTest {
+
+    public volatile boolean flag = true;
+
+    public static void main(String[] args) throws InterruptedException {
+        VolatileTest demo = new VolatileTest();
+        System.out.println("代码开始了+++++");
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i = 0;
+                while (demo.flag) {
+                    i++;
+                }
+                System.out.println(i);
+            }
+        }).start();
+
+        TimeUnit.SECONDS.sleep(2);
+
+        demo.flag = false;
+
+        System.out.println("被置为false了+++++++");
+    }
+}
