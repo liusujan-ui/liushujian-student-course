3e8ffafe525d5404f5df0cb3633d424b47a8a1d2
liusujan-ui <liusujan6@gmail.com>
2026-07-18 20:12:54 +0800

    20260718 v0.0.0.1 开始大数据方面的代码学习

---
+++ bigdata_study/src/main/java/com/tencent/bigdata_study/BigdataStudyApplication.java
@@ -0,0 +1,13 @@
+package com.tencent.bigdata_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigdataStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigdataStudyApplication.class, args);
+    }
+
+}
